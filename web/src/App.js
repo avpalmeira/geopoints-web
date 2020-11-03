d0bd9fe374d8609e5d9e3944f65e9791ec1d8974
Antonio Palmeira <av.palmeira@gmail.com>
2020-11-03 14:08:17 -0300

    navigate app to different content

--- web/src/App.js
+++ web/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Layout, Menu, Breadcrumb } from 'antd';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Map from './components/Map';
 
 import 'antd/dist/antd.css';
@@ -10,22 +11,32 @@ function App() {
 
 	return (
 		<Layout className="layout">
-			<Header className="top-nav">
-				<div className="logo" />
-				<Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
-					<Menu.Item key="1">Pontos Geográficos</Menu.Item>
-					<Menu.Item key="2">Importar CSV</Menu.Item>
-				</Menu>
-			</Header>
-			<Content style={{ padding: '0 50px' }}>
-				<Breadcrumb style={{ margin: '16px 0' }}>
-					<Breadcrumb.Item>Home</Breadcrumb.Item>
-					<Breadcrumb.Item>App</Breadcrumb.Item>
-				</Breadcrumb>
-				<div className="site-layout-content">
-          <Map />
-        </div>
-			</Content>
+      <Router>
+        <Header className="top-nav">
+          <div className="logo" />
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1">
+              <Link to="/">Pontos Geográficos</Link>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Link to="/import">Importar CSV</Link>
+            </Menu.Item>
+          </Menu>
+        </Header>
+        
+        <Content style={{ padding: '0 50px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>App</Breadcrumb.Item>
+            <Breadcrumb.Item>Maps</Breadcrumb.Item>
+          </Breadcrumb>
+          <div className="site-layout-content">
+            <Switch>
+              <Route path="/import"><h1>Import CSV file</h1></Route>
+              <Route path="/"><Map/></Route>
+            </Switch>
+          </div>
+        </Content>
+      </Router>
 			<Footer style={{ textAlign: 'center' }}>Made with {"<3"}</Footer>
 		</Layout>
 	);
